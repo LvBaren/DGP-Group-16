ce10e6cfb2e9e6ab5d6b250f4da3b1612dadbdb2
LvBaren <131879536+LvBaren@users.noreply.github.com>
2025-09-29 11:22:52 +0200

    Added key arrow directions
    Wasd walking now also possible with arrow keys

--- DGP group 16/Assets/Scripts/Player.cs
+++ DGP group 16/Assets/Scripts/Player.cs
@@ -14,22 +14,22 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             inputVector.y = +1;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             inputVector.y = -1;
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             inputVector.x = -1;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             inputVector.x = +1;
         }
