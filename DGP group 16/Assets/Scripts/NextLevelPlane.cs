bdb4edb9684d3c6e24a0910822a6ef38c18d34d0
J-Haarsma <j.haarsma1@student.tue.nl>
2025-10-27 10:50:32 +0100

    Aanpassingen oa Crossfade

--- DGP group 16/Assets/Scripts/NextLevelPlane.cs
+++ DGP group 16/Assets/Scripts/NextLevelPlane.cs
@@ -1,3 +1,39 @@
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class NextLevelPlane : MonoBehaviour
+//{
+//    [Header("Scene to load on collision")]
+//    public string sceneName; // Set this in the Inspector
+
+//    [Header("Optional: Custom spawn point tag or name for next scene")]
+//    public string nextSpawnTag = ""; // leave empty to use default "PlayerSpawn"
+
+//    void OnCollisionEnter(Collision collision)
+//    {
+//        if (collision.gameObject.CompareTag("Player"))
+//        {
+//            LoadNextScene();
+//        }
+//    }
+
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            LoadNextScene();
+//        }
+//    }
+
+//    private void LoadNextScene()
+//    {
+//        // Sla het gewenste spawnpoint op, zodat de PersistentPlayer het weet
+//        PersistentPlayer.NextSpawnTag = nextSpawnTag;
+
+//        SceneManager.LoadScene(sceneName);
+//    }
+//}
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +44,10 @@ public class NextLevelPlane : MonoBehaviour
 
     [Header("Optional: Custom spawn point tag or name for next scene")]
     public string nextSpawnTag = ""; // leave empty to use default "PlayerSpawn"
+
+    [SerializeField] Animator transition;
+
+    public float transitionTime = 1.0f;
 
     void OnCollisionEnter(Collision collision)
     {
@@ -27,9 +67,20 @@ public class NextLevelPlane : MonoBehaviour
 
     private void LoadNextScene()
     {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
         // Sla het gewenste spawnpoint op, zodat de PersistentPlayer het weet
         PersistentPlayer.NextSpawnTag = nextSpawnTag;
 
+        yield return new WaitForSeconds(transitionTime);
+
         SceneManager.LoadScene(sceneName);
+
+        //transition.SetTrigger("Start");
     }
 }
